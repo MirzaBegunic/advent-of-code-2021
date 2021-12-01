d492b4cdbde76b4641a460f7906641b479a9599c
Mirza Begunic <mirza.begunic.97@gmail.com>
2021-12-02 00:28:34 +0100

    Day 1: Sonar Sweep

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,50 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val depthValues = input.map { it.toInt() }
+
+        var depthIncreasedFromPreviousDepthCounter = 0
+
+        for (depthCollectionCounter: Int in 1 until depthValues.size) {
+            val currentDepth = depthValues[depthCollectionCounter]
+            val previousDepth = depthValues[depthCollectionCounter-1]
+            if (currentDepth > previousDepth) {
+                depthIncreasedFromPreviousDepthCounter++
+            }
+        }
+
+        return depthIncreasedFromPreviousDepthCounter
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var depthSumIncreasedFromPreviousDepthSumCounter = 0
+
+        val depthValues = input.map { it.toInt() }
+
+        for (depthCollectionCounter: Int in 1..depthValues.size-3) {
+            val previousDepthSum = depthValues.subList(depthCollectionCounter-1, depthCollectionCounter+2).sum()
+            val currentDepthSum = depthValues.subList(depthCollectionCounter, depthCollectionCounter+3).sum()
+            if (currentDepthSum > previousDepthSum) {
+                depthSumIncreasedFromPreviousDepthSumCounter++
+            }
+        }
+        return depthSumIncreasedFromPreviousDepthSumCounter
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
+    // check input
+    if (input.isEmpty()) {
+        throw IllegalArgumentException("Input cannot be empty")
+    }
+    // print results
     println(part1(input))
     println(part2(input))
+
+    // correct results from my input data
+    // 1766
+    // 1797
 }
