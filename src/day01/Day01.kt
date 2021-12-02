38fb50e2b9c2d7ad1b376d4dc48b6327c5c1ff9f
Mirza Begunic <mirza.begunic.97@gmail.com>
2021-12-02 23:57:29 +0100

    Move day 1 solution to its own package

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -1,3 +1,7 @@
+package day01
+
+import readInput
+
 fun main() {
     fun part1(input: List<String>): Int {
         val depthValues = input.map { it.toInt() }
@@ -31,11 +35,11 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = readInput("day01/Day01_test")
     check(part1(testInput) == 7)
     check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInput("day01/Day01")
     // check input
     if (input.isEmpty()) {
         throw IllegalArgumentException("Input cannot be empty")
