1ffef2114b1b824c5a34a7647edec7b3d8d7f3b2
Mirza Begunic <mirza.begunic.97@gmail.com>
2021-12-03 00:41:38 +0100

    Refactor Day 1

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -3,37 +3,6 @@ package day01
 import readInput
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        val depthValues = input.map { it.toInt() }
-
-        var depthIncreasedFromPreviousDepthCounter = 0
-
-        for (depthCollectionCounter: Int in 1 until depthValues.size) {
-            val currentDepth = depthValues[depthCollectionCounter]
-            val previousDepth = depthValues[depthCollectionCounter-1]
-            if (currentDepth > previousDepth) {
-                depthIncreasedFromPreviousDepthCounter++
-            }
-        }
-
-        return depthIncreasedFromPreviousDepthCounter
-    }
-
-    fun part2(input: List<String>): Int {
-        var depthSumIncreasedFromPreviousDepthSumCounter = 0
-
-        val depthValues = input.map { it.toInt() }
-
-        for (depthCollectionCounter: Int in 1..depthValues.size-3) {
-            val previousDepthSum = depthValues.subList(depthCollectionCounter-1, depthCollectionCounter+2).sum()
-            val currentDepthSum = depthValues.subList(depthCollectionCounter, depthCollectionCounter+3).sum()
-            if (currentDepthSum > previousDepthSum) {
-                depthSumIncreasedFromPreviousDepthSumCounter++
-            }
-        }
-        return depthSumIncreasedFromPreviousDepthSumCounter
-    }
-
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day01/Day01_test")
     check(part1(testInput) == 7)
@@ -51,4 +20,35 @@ fun main() {
     // correct results from my input data
     // 1766
     // 1797
+}
+
+fun part1(input: List<String>): Int {
+    val depthValues = input.map { it.toInt() }
+
+    var depthIncreasedFromPreviousDepthCounter = 0
+
+    for (depthCollectionCounter: Int in 1 until depthValues.size) {
+        val currentDepth = depthValues[depthCollectionCounter]
+        val previousDepth = depthValues[depthCollectionCounter-1]
+        if (currentDepth > previousDepth) {
+            depthIncreasedFromPreviousDepthCounter++
+        }
+    }
+
+    return depthIncreasedFromPreviousDepthCounter
+}
+
+fun part2(input: List<String>): Int {
+    var depthSumIncreasedFromPreviousDepthSumCounter = 0
+
+    val depthValues = input.map { it.toInt() }
+
+    for (depthCollectionCounter: Int in 1..depthValues.size-3) {
+        val previousDepthSum = depthValues.subList(depthCollectionCounter-1, depthCollectionCounter+2).sum()
+        val currentDepthSum = depthValues.subList(depthCollectionCounter, depthCollectionCounter+3).sum()
+        if (currentDepthSum > previousDepthSum) {
+            depthSumIncreasedFromPreviousDepthSumCounter++
+        }
+    }
+    return depthSumIncreasedFromPreviousDepthSumCounter
 }
